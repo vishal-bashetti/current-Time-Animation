abe7089b27823c809cf96f0871d849b29d6879d5
vishal-bashetti <vishalabashetti@gmail.com>
2021-09-21 16:15:41 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,371 @@
+const numbers = [
+  //0
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 6, minute: 00 },
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+
+  // 1
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 6, minute: 45 },
+    { hour: 7.5, minute: 31.5 },
+
+    { hour: 3, minute: 0 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 0 },
+    { hour: 7.5, minute: 31.5 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+    { hour: 7.5, minute: 31.5 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+    { hour: 7.5, minute: 31.5 },
+
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 00 },
+    { hour: 3, minute: 00 },
+    { hour: 6, minute: 45 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  // 2
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 3, minute: 0 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 3, minute: 00 },
+    { hour: 3, minute: 45 },
+    { hour: 6, minute: 45 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  // 3
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 3, minute: 0 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 15 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  //4
+  [
+    { hour: 3, minute: 30 },
+    { hour: 6, minute: 45 },
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 30 },
+
+    { hour: 6, minute: 00 },
+    { hour: 12, minute: 30 },
+    { hour: 12, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 12, minute: 15 },
+    { hour: 9, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 12, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  // 5
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 6, minute: 0 },
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 15 },
+    { hour: 0, minute: 45 },
+
+    { hour: 6, minute: 0 },
+    { hour: 0, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 6, minute: 45 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 15 },
+    { hour: 3, minute: 45 },
+    { hour: 0, minute: 45 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  // 6
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 6, minute: 0 },
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 15 },
+    { hour: 0, minute: 45 },
+
+    { hour: 6, minute: 0 },
+    { hour: 0, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 6, minute: 45 },
+
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 3, minute: 00 },
+    { hour: 0, minute: 45 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  //7
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 3, minute: 00 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 12, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  // 8
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 6, minute: 0 },
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 30 },
+    { hour: 0, minute: 30 },
+
+    { hour: 5, minute: 0 },
+    { hour: 0, minute: 15 },
+    { hour: 9, minute: 00 },
+    { hour: 7, minute: 00 },
+
+    { hour: 1, minute: 30 },
+    { hour: 6, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 11, minute: 30 },
+
+    { hour: 6, minute: 00 },
+    { hour: 3, minute: 00 },
+    { hour: 0, minute: 45 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+  //9
+  [
+    { hour: 3, minute: 30 },
+    { hour: 3, minute: 45 },
+    { hour: 3, minute: 45 },
+    { hour: 9, minute: 30 },
+
+    { hour: 6, minute: 00 },
+    { hour: 3, minute: 30 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 6, minute: 00 },
+    { hour: 12, minute: 15 },
+    { hour: 9, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 3, minute: 00 },
+    { hour: 9, minute: 15 },
+    { hour: 9, minute: 30 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 6, minute: 00 },
+    { hour: 6, minute: 00 },
+
+    { hour: 7.5, minute: 31.5 },
+    { hour: 7.5, minute: 31.5 },
+    { hour: 12, minute: 15 },
+    { hour: 9, minute: 00 },
+  ],
+];
+
+const hr1 = document.getElementsByClassName("hr1");
+const hr2 = document.getElementsByClassName("hr2");
+const hr3 = document.getElementsByClassName("hr3");
+const hr4 = document.getElementsByClassName("hr4");
+
+const mn1 = document.getElementsByClassName("mn1");
+const mn2 = document.getElementsByClassName("mn2");
+const mn3 = document.getElementsByClassName("mn3");
+const mn4 = document.getElementsByClassName("mn4");
+
+const date = new Date();
+let timeHour = date.getHours().toLocaleString().split("");
+let timeMinute = date.getMinutes().toLocaleString().split("");
+
+let hour1 = timeHour[0];
+let hour2 = timeHour[1];
+let minute1 = timeMinute[0];
+let minute2 = timeMinute[1];
+
+animation = (hour1, hour2, minute1, minute2) => {
+  for (let i = 0; i < numbers[0].length; i++) {
+    let hh1 = numbers[hour1][i].hour * 30;
+    let hh2 = numbers[hour2][i].hour * 30;
+    let hh3 = numbers[minute1][i].hour * 30;
+    let hh4 = numbers[minute2][i].hour * 30;
+
+    let mm1 = numbers[hour1][i].minute * 30;
+    let mm2 = numbers[hour2][i].minute * 30;
+    let mm3 = numbers[minute1][i].minute * 30;
+    let mm4 = numbers[minute2][i].minute * 30;
+
+    hr1[i].style.transform = `rotateZ(${hh1 + 360}deg)`;
+    hr2[i].style.transform = `rotateZ(${hh2 + 360}deg)`;
+    hr3[i].style.transform = `rotateZ(${hh3 + 360}deg)`;
+    hr4[i].style.transform = `rotateZ(${hh4 + 360}deg)`;
+
+
+
+    mn1[i].style.transform = `rotateZ(${mm1 + 360}deg)`;
+    mn2[i].style.transform = `rotateZ(${mm2 + 360}deg)`;
+    mn3[i].style.transform = `rotateZ(${mm3 + 360}deg)`;
+    mn4[i].style.transform = `rotateZ(${mm4 + 360}deg)`;
+
+  }
+};
+console.log(hour1);
+animation(hour1, hour2, minute1, minute2);
