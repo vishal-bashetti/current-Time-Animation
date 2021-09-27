6042a2dfff3f976e04439ac95ad1d02bd344ea10
vishal-bashetti <vishalabashetti@gmail.com>
2021-09-27 21:24:05 +0530

    Fixed timer

--- app.js
+++ app.js
@@ -332,52 +332,58 @@ const mn2 = document.getElementsByClassName("mn2");
 const mn3 = document.getElementsByClassName("mn3");
 const mn4 = document.getElementsByClassName("mn4");
 
-const date = new Date();
-let timeHour = date.getHours().toLocaleString().split("");
-let timeMinute = date.getMinutes().toLocaleString().split("");
+function getDate() {
+  const date = new Date();
+  console.log(date);
+  let timeHour = date.getHours().toLocaleString().split("");
+  let timeMinute = date.getMinutes().toLocaleString().split("");
 
-let hour1;
-let hour2;
+  let hour1;
+  let hour2;
 
-let minute1;
-let minute2;
+  let minute1;
+  let minute2;
 
-if (timeHour.length === 1) {
-  hour1 = 0;
-  hour2 = timeHour[0];
-} else {
-  hour1 = timeHour[0];
-  hour2 = timeHour[1];
-}
-console.log(timeMinute);
-if (timeMinute.length === 1) {
-  minute1 = 0;
-  minute2 = timeMinute[0];
-} else {
-  minute1 = timeMinute[0];
-  minute2 = timeMinute[1];
-}
-animation = (hour1, hour2, minute1, minute2) => {
-  for (let i = 0; i < numbers[0].length; i++) {
-    let hh1 = numbers[hour1][i].hour * 30;
-    let hh2 = numbers[hour2][i].hour * 30;
-    let hh3 = numbers[minute1][i].hour * 30;
-    let hh4 = numbers[minute2][i].hour * 30;
-
-    let mm1 = numbers[hour1][i].minute * 30;
-    let mm2 = numbers[hour2][i].minute * 30;
-    let mm3 = numbers[minute1][i].minute * 30;
-    let mm4 = numbers[minute2][i].minute * 30;
-
-    hr1[i].style.transform = `rotateZ(${hh1 + 360}deg)`;
-    hr2[i].style.transform = `rotateZ(${hh2 + 360}deg)`;
-    hr3[i].style.transform = `rotateZ(${hh3 + 360}deg)`;
-    hr4[i].style.transform = `rotateZ(${hh4 + 360}deg)`;
-
-    mn1[i].style.transform = `rotateZ(${mm1 + 360}deg)`;
-    mn2[i].style.transform = `rotateZ(${mm2 + 360}deg)`;
-    mn3[i].style.transform = `rotateZ(${mm3 + 360}deg)`;
-    mn4[i].style.transform = `rotateZ(${mm4 + 360}deg)`;
+  if (timeHour.length === 1) {
+    hour1 = 0;
+    hour2 = timeHour[0];
+  } else {
+    hour1 = timeHour[0];
+    hour2 = timeHour[1];
   }
-};
-animation(hour1, hour2, minute1, minute2);
+  if (timeMinute.length === 1) {
+    minute1 = 0;
+    minute2 = timeMinute[0];
+  } else {
+    minute1 = timeMinute[0];
+    minute2 = timeMinute[1];
+  }
+
+  animation = (hour1, hour2, minute1, minute2) => {
+    for (let i = 0; i < numbers[0].length; i++) {
+      let hh1 = numbers[hour1][i].hour * 30;
+      let hh2 = numbers[hour2][i].hour * 30;
+      let hh3 = numbers[minute1][i].hour * 30;
+      let hh4 = numbers[minute2][i].hour * 30;
+
+      let mm1 = numbers[hour1][i].minute * 30;
+      let mm2 = numbers[hour2][i].minute * 30;
+      let mm3 = numbers[minute1][i].minute * 30;
+      let mm4 = numbers[minute2][i].minute * 30;
+
+      hr1[i].style.transform = `rotateZ(${hh1 + 360}deg)`;
+      hr2[i].style.transform = `rotateZ(${hh2 + 360}deg)`;
+      hr3[i].style.transform = `rotateZ(${hh3 + 360}deg)`;
+      hr4[i].style.transform = `rotateZ(${hh4 + 360}deg)`;
+
+      mn1[i].style.transform = `rotateZ(${mm1 + 360}deg)`;
+      mn2[i].style.transform = `rotateZ(${mm2 + 360}deg)`;
+      mn3[i].style.transform = `rotateZ(${mm3 + 360}deg)`;
+      mn4[i].style.transform = `rotateZ(${mm4 + 360}deg)`;
+    }
+  };
+
+  animation(hour1, hour2, minute1, minute2);
+}
+
+setInterval(getDate, 1000);
